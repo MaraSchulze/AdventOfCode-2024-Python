16f27b46c3296a8bd25169e89f8bc0fe6a4d8935
Mara Schulze <s8mrschu@stud.uni-saarland.de>
2024-12-03 09:16:19 +0100

    More elegant solution for day 3 riddle 2

--- riddles/day3.2.py
+++ riddles/day3.2.py
@@ -11,32 +11,31 @@ s = "".join(inp)
 
 # get list of matches
 pattern = r"mul\(\d{1,3},\d{1,3}\)"
-matches = re.finditer(pattern, s)
+matches_it = re.finditer(pattern, s)
 
 # get do-dont intervals
-dos_matches = re.finditer("do\(\)", s)
-donts_matches = re.finditer("don't\(\)", s)
+dos_it = re.finditer("do\(\)", s)
+donts_it = re.finditer("don't\(\)", s)
 
-dos = [match.start() for match in dos_matches] + [0]
-donts = [match.start() for match in donts_matches] + [len(s)]
+# combine all 3 iterators into one labeled list
+intervals = [("mul", match.start(), match.group()) for match in matches_it]
+intervals += [("do", match.start()) for match in dos_it]
+intervals += [("dont", match.start()) for match in donts_it]
 
-dos.sort()
-donts.sort()
-
-intervals = []
-index = 0
-for start in dos:
-	while donts[index] < start:
-		index += 1
-	intervals.append((start, donts[index]))
+# sort intervals after 2nd projection
+intervals.sort(key=lambda x: x[1])
 
 # sum up
+allow = True
 result = 0
-for match in matches:
-	for start, end in intervals:
-		if start < match.start() < end:
-			result += eval(match.group())
-			break
+for point in intervals:
+	if point[0] == "do":
+		allow = True
+	elif point[0] == "dont":
+		allow = False
+	else:
+		if allow == True:
+			result += eval(point[2])
 
 # print result
 print(result)
